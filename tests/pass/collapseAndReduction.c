39343b21c68e5ac49874da23c22c1f1398fd31b0
jeremy-friesen <jeremy.friesen@ontariotechu.net>
2022-03-13 18:04:19 -0400

    Add message that suggests pragma/parameters. Make collapse logic determine collapse 'depth'. Fix issue with finding reduction opportunities in nested loops

---
+++ tests/pass/collapseAndReduction.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int sum = 0;
+
+    //@omp-analysis=true
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            sum += i * j;
+        }
+    }
+}
